e4d567ea829bbe89b0770ac3bfdc54348918d30c
alanasim <alana.simoncelli@gmail.com>
2016-06-17 11:45:00 -0400

    followed Sophie's directions for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :arts
   has_many :spaces
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
       resources :users, except: [:new, :edit]
     end
   end
+  post '/login', to: 'sessions#create'
 end
