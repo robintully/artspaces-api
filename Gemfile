3672991ae5689c9c0031bacaa40682af23c8f7ee
alanasim <alana.simoncelli@gmail.com>
2016-06-17 15:38:52 -0400

    working authorization

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'active_model_serializers'
 gem 'rack-cors'
 gem 'jwt'
 gem 'figaro'
+gem 'pry-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/api/v1/arts_controller.rb
+++ app/controllers/api/v1/arts_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class ArtsController < ApplicationController
       def index
+        binding.pry
         render json: Art.includes(:user, :spaces), include: ['user', 'spaces']
       end
     end
