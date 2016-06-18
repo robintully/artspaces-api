4e8d811b4dc0a31ac9ef66b40a78e6fd6bd0a834
robintully <mr.robintully@gmail.com>
2016-06-18 17:57:13 -0400

    $

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email
+  has_many :spaces
+  has_many :arts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :arts, except: [:new, :edit]
       resources :spaces, except: [:new, :edit]
       resources :users, except: [:new, :edit]
+       resources :currentusers, except: [:new, :edit]
     end
   end
   post '/login', to: 'sessions#create'
