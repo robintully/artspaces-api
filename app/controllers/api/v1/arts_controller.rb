0b157a0c40c63d6ea5d8ebbb052eb6dc1939386d
alanasim <alana.simoncelli@gmail.com>
2016-06-20 00:17:03 -0400

    currentusers testing and art create working functionality

--- app/controllers/api/v1/arts_controller.rb
+++ app/controllers/api/v1/arts_controller.rb
@@ -4,6 +4,15 @@ module Api
       def index
         render json: Art.includes(:user, :spaces), include: ['user', 'spaces']
       end
+
+      def create
+        Art.create(name: art_params[:attributes][:name], description: art_params[:attributes][:description], img_link: art_params[:attributes]["img-link"], user: current_user)
+      end
+
+      private
+      def art_params
+        params.require(:data).permit(:attributes => [:name, :description, :"img-link"])
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/arts', to: 'arts#create'
       resources :arts, except: [:new, :edit]
       resources :spaces, except: [:new, :edit]
+      get 'users/current-user', to: 'currentusers#index'
       resources :users, except: [:new, :edit]
-       resources :currentusers, except: [:new, :edit]
+      resources :currentusers, except: [:new, :edit]
     end
   end
   post '/login', to: 'sessions#create'
